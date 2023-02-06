8d054b6eca0c29de097c3135161b2133c7fdc635
Yosef Hershberg <Berioo2000@gmail.com>
2023-02-06 11:12:44 +0200

    before implementing context

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import notesSlice from "../features/slices/notesSlice";
 import displaydNoteSlice from "../features/slices/displaydNoteSlice";
-import displaydFolder from "../features/slices/displaydFolderSlice";
-
+import displaydFolderSlice from "../features/slices/displaydFolderSlice";
 
 export const store = configureStore({
   reducer: {
     notes: notesSlice,
     displaydNote: displaydNoteSlice,
-    displaydFolder: displaydFolder
+    displaydFolder: displaydFolderSlice,
   },
 });
